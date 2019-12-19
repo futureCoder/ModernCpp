f74a197583422498749a6e8c919b2cffd1d0390f
jasondong <jason-dong@foxmail.com>
2019-12-19 22:09:54 +0800

    update

--- ModernCppTest/main.cpp
+++ ModernCppTest/main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <utility>
+#include <functional>
 
 using namespace std;
 
@@ -22,7 +23,7 @@ namespace Noah
         A(const A&)
         {
             cout << "Copy A\n";
-        } 
+        }
         A(A&&)
         {
             cout << "Move A\n";
@@ -44,6 +45,11 @@ namespace Noah
 class MyString : public string
 {
 public:
+    MyString() noexcept
+        : string()
+    {
+        cout << "MyString" << endl;
+    }
     MyString(const char* p) noexcept
         : string(p)
     {
@@ -87,10 +93,105 @@ public:
     }
 };
 
+class TestFunc
+{
+public:
+    int Output(int a, int b)
+    {
+        cout << a << " " << b << endl;
+        return 0;
+    }
+};
+
+int Output(int a, int b/*, int c, int d*/)
+{
+    cout << a << " " << b << endl;
+    return 0;
+}
+
+struct Foo {
+    Foo(int num) : num_(num) {}
+    void print_add(int i) const { std::cout << num_ + i << '\n'; }
+    int num_;
+};
+
+void print_num(int i)
+{
+    std::cout << i << '\n';
+}
+
+struct PrintNum {
+    void operator()(int i) const
+    {
+        std::cout << i << '\n';
+    }
+};
+
+
+class CTest
+
+{
+public:
+    int InternalFunc(int a, int b)
+    {
+        cout << a + b << endl;
+        return a + b;
+    }
+private:
+
+};
+
+typedef int(CTest::*FUNC)(int, int);
+
 int main()
 {
-    //Noah::main();
-    MyString a = "abc";
-    MyString b = a;
+    ////TestFunc a;
+    ////auto fun = std::bind(Output, 2, placeholders::_1);
+    //using std::placeholders::_1;
+    //using std::placeholders::_2;
+    //using std::placeholders::_3;
+    //using std::placeholders::_4;
+    //using std::placeholders::_10;
+    //using std::placeholders::_20;
+
+
+
+    //std::function < int(int ,int) > a = std::bind(Output, _2, 60);
+    //a(111,222);
+
+    // store a free function
+    //std::function<void(int)> f_display = print_num;
+    //f_display(-9);
+
+    //// store a lambda
+    //std::function<void()> f_display_42 = []() { print_num(42); };
+    //f_display_42();
+
+    //// store the result of a call to std::bind
+    //std::function<void()> f_display_31337 = std::bind(print_num, 31337);
+    //f_display_31337();
+
+    //// store a call to a member function
+    //std::function<void(const Foo&, int)> f_add_display = &Foo::print_add;
+    //const Foo foo(314159);
+    //f_add_display(foo, 1);
+    //f_add_display(314159, 1);
+
+    //// store a call to a data member accessor
+    //std::function<int(Foo const&)> f_num = &Foo::num_;
+    //std::cout << "num_: " << f_num(foo) << '\n';
+
+    //// store a call to a member function and object
+    //
+    //std::function<void(int)> f_add_display2 = std::bind(&Foo::print_add, foo, _1);
+    //f_add_display2(2);
+
+    //// store a call to a member function and object ptr
+    //std::function<void(int)> f_add_display3 = std::bind(&Foo::print_add, &foo, _1);
+    //f_add_display3(3);
+
+    //// store a call to a function object
+    //std::function<void(int)> f_display_obj = PrintNum();
+    //f_display_obj(18);
     return 0;
 }
